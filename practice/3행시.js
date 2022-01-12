5cd9331711f299dcbc3dddece5882d7dc8c04db8
abbiddo@naver.com <abbiddo@naver.com>
2022-01-12 10:19:22 +0900

    Third day - JS prac 3행시

---
+++ practice/3행시.js
@@ -0,0 +1,14 @@
+function change0(){
+	var h1=document.getElementsByTagName('h1');
+	h1[0].innerHTML="모여서";
+}
+
+function change1(){
+	var h1=document.getElementsByTagName('h1');
+	h1[1].innerHTML="각자";
+}
+
+function change2(){
+	var h1=document.getElementsByTagName('h1');
+	h1[2].innerHTML="코딩";
+}
