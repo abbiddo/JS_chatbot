5577dd3fc0fe089542205a81b3f6eeb530b9849b
abbiddo@naver.com <abbiddo@naver.com>
2022-01-13 10:20:16 +0900

    4th day - console

--- practice/3행시.js
+++ practice/3행시.js
@@ -1,14 +1,19 @@
+var count=0;
+
 function change0(){
 	var h1=document.getElementsByTagName('h1');
 	h1[0].innerHTML="모여서";
+	console.log(count++);
 }
 
 function change1(){
 	var h1=document.getElementsByTagName('h1');
 	h1[1].innerHTML="각자";
+	console.log(count++);
 }
 
 function change2(){
 	var h1=document.getElementsByTagName('h1');
 	h1[2].innerHTML="코딩";
+	console.log(count++);
 }
