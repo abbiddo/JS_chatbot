51323aa86b97b08611785165b6e1a1a54fcd5f25
abbiddo@naver.com <abbiddo@naver.com>
2022-01-15 22:31:45 +0900

    claculator

--- 계산기/cal.js
+++ 계산기/cal.js
@@ -1,45 +1,45 @@
-var n1=0,n2=0;
+var n1=0,n2=0,cnt=0;
 var s=0,d=0,i=1;
 var str="";
 var h2=document.getElementsByTagName('h2');
 
 function num(n){
-	h2[0].innerHTML=str;
+	if (cnt>0) {
+		n1=0;
+		str="";
+		cnt=0;
+	}
 
+	h2[0].innerHTML=str;
 	str+=n;
 
 	if (s==0){
-		if (d==0){
-			n1*=10;
-			n1+=n;
-		}
+		if (d==0) n1*=10;
 		else{
 			i*=0.1;
 			n*=i;
-			n1+=n;
-		}		
+		}	
+		n1+=n;	
 	}
 
 	else {
-		if (d==0){
-			n2*=10;
-			n2+=n;
-		}
+		if (d==0) n2*=10;
 		else{
 			i*=0.1;
 			n*=i;
-			n2+=n;
 		}
+		n2+=n;
 	}
-
+	
 	h2[0].innerHTML=str;
 }
 
 function symbol(n){
 	s=n;
+	cnt=0;
 	if (s==1) str+="+";
 	else if (s==2) str+="-";
-	else if (s==3) str+="*";
+	else if (s==3) str+="X";
 	else str+="/";
 
 	h2[0].innerHTML=str;
@@ -55,33 +55,21 @@ function dot(){
 }
 
 function result(){
+	cnt+=1;
 	str+="=";
-	if (s==1){
-		str+=n1+n2;
-		console.log(n1+n2);
-	}
+	
+	if (s==1) n1+=n2;
+	else if (s==2) n1-=n2;
+	else if (s==3) n1*=n2;
+	else n1/=n2;
 
-	else if (s==2){
-		str+=n1-n2;
-		console.log(n1-n2);
-	}
-
-	else if (s==3){
-		str+=n1*n2;
-		console.log(n1*n2);
-	}
-
-	else {
-		str+=n1/n2;
-		console.log(n1/n2);
-	}
-
+	str+=n1;
+	console.log(n1);
 	h2[0].innerHTML=str;
 
-	n1=0;
 	n2=0;
 	s=0;
-	str="";
+	str=n1;
 	d=0;
 	i=1;
 }
