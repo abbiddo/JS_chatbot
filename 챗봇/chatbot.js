c23cfd8cae6e84a99afb54a8eed1ffc556e60f8b
abbiddo@naver.com <abbiddo@naver.com>
2022-01-21 19:30:21 +0900

    12th day - chatbot communication

--- 챗봇/chatbot.js
+++ 챗봇/chatbot.js
@@ -1,23 +1,35 @@
-var no={
-	0:"먹을거야",
-	1:"맛있겠다",
-	2:"먹어야지",
-	3:"먹고싶다",
-	4:"먹고싶어"
-}
-
 var h2=document.getElementsByTagName("h2");
+var k=1,kk=0;
 
 function text_input(){
 	var value = document.getElementById("console").value;
-	var i,n=0;
-
-	for (i=0;i<5;i++)
-		if (value==no[i]){
-			h2[0].innerHTML="❌안 돼❌";
-			alert("❗❗❌❌위험❌❌❗❗");
-			n=1;
-			break;
+	if (k!=0) {
+		if (value=="하하호호"){
+			h2[0].innerHTML="안녕하세요! 어떤 도움이 필요하신가요?"
+			k=0;
+		}
 	}
-	if (n==0) h2[0].innerHTML=value;
+
+	else if (value.includes("맛있")||value.includes("먹")){
+		h2[0].innerHTML="❌안 돼❌";
+		alert("❗❗❌❌위험❌❌❗❗");
+	}
+
+	else if (value=="불 꺼") {
+		h2[0].innerHTML="ㅇㅋ";
+		document.body.style.backgroundColor="black";
+	}
+
+	else if (value=="불 켜") {
+		h2[0].innerHTML="ㅇㅋ";
+		document.body.style.backgroundColor="white";
+	}
+
+	else h2[0].innerHTML=value;
+}
+
+function help(){
+	kk++;
+	if (kk%2==1) document.getElementById("help").style.display="block";
+	else document.getElementById("help").style.display="none";
 }
