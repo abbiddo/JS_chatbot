9771ab63630afee0f618d07e797a63e9d3297468
abbiddo@naver.com <abbiddo@naver.com>
2022-01-27 01:04:25 +0900

    15th day - chatbot - learning answer

--- 챗봇/chatbot.js
+++ 챗봇/chatbot.js
@@ -1,3 +1,20 @@
+var json=[
+	{
+		"question":"안녕",
+		"answer":"안호!"
+	},
+	{
+		"question":"나이",
+		"answer":"그런거 없다 호!"
+	},
+	{
+		"question":"이름",
+		"answer":"호호!"
+	},
+]
+var question="";
+var answer="";
+var key=0;
 var h2=document.getElementsByTagName("h2");
 var k=0,kk=0,l=0;
 var str="",set;
@@ -7,7 +24,7 @@ function text_input(){
 	if (k==0) {
 		if (value=="하하호호"){
 			k=1;			
-			str="안녕하세요! 어떤 도움이 필요하신가요?";
+			str="안녕하세요! 어떤 도움이 필요하신가호?";
 			typing();
 		}
 	}
@@ -38,7 +55,7 @@ function text_input(){
 
 	else if (value=="불 꺼") {
 		if (l==1) {
-			str="꺼져 있잖아^^";
+			str="꺼져 있잖호^^";
 			typing();
 		}
 		else{
@@ -51,7 +68,7 @@ function text_input(){
 
 	else if (value=="불 켜") {
 		if (l==0) {
-			str="켜져 있잖아^^";
+			str="켜져 있잖호^^";
 			typing();
 		}
 		else {
@@ -62,10 +79,42 @@ function text_input(){
 		}
 	}
 
-	else {
-		str="호~";
-		typing();
+	else text_check(value)
+}
+
+function text_check(value){
+	if (key==1){
+		if(value=="네"){
+			str="대답을 입력해주세호";
+			typing();
+			key=2;
+		}
+		else{
+			str="호 :(";
+			typing();
+			key=0;
+		}
+		return 0;
 	}
+
+	if (key==2){
+		answer=value;
+		push_json()
+		return 0;
+	}
+
+	for (let i=0;i<json.length;i++){
+		if (value==json[i].question){
+			str=json[i].answer;
+			typing();
+			return;
+		}
+	}
+
+	str="말을 가르쳐 주실래호?  (네 or 아니요)"
+	typing();
+	question=value
+	key=1;
 }
 
 function help(){
@@ -90,5 +139,11 @@ function typing(){
 		return 0;
 	}
 	
-	setTimeout(typing,300);
+	setTimeout(typing,100);
+}
+
+function push_json(){
+	json.push({question: `${question}`, answer: `${answer}`});
+	h2[0].innerHTML = "말을 배웠호!";
+	key = 0;
 }
